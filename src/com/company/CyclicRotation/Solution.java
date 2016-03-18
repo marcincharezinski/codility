a67b84b6e938bc279ac06928e18f68701f4d87ac
marcincharezinski <marcin.charezinski@gmail.com>
2016-03-18 11:41:03 +0100

    correct solution

--- src/com/company/CyclicRotation/Solution.java
+++ src/com/company/CyclicRotation/Solution.java
@@ -1,38 +1,30 @@
-package com.company.cyclicRotation;
+package com.company.CyclicRotation;
 
-
-import java.util.Arrays;
 
 public class Solution {
 
     public static void main(String[] args) {
-        int[] before = {3,8,9,7,6};
-        int[] after = new int[before.length];
+
         int K = 3;
+        int[] A = {};
+        solution(A, K);
+
+    }
 
 
+    public static int[] solution(int[] A, int K) {
+        if (A.length == 0) return A;
+        for (int j = 0; j < K; j++) {
+            int[] after = new int[A.length];
 
-    for(int j = 0; j<K; j++) {
+            for (int i = 1; i < A.length; i++) {
+                after[i] = A[i - 1];
+            }
+            after[0] = A[A.length - 1];
+            A = after;
 
-        for (int i = 1; i < before.length; i++) {
-            after[i] = before[i - 1];
         }
-        after[0] = before[before.length - 1];
 
-        System.out.println("after: " + Arrays.toString(after));
-
-    }
-
-    }
-
-
-
-    public int[] solution(int[] A, int K) {
-
-
-
-
-
-        return null;
+        return A;
     }
 }
